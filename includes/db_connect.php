461b8e733c6e4f4b36fb53842a31a0feb52ebfff
Sayyid RafeeD <msayyid.rafeed@gmail.com>
2025-11-08 15:00:12 +0700

    feat: database config

---
+++ includes/db_connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";       
+$password = "";           
+$dbname = "db_kantin_upnvj"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Koneksi Gagal: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+?>
