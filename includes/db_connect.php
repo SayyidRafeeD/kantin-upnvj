9244856eb9dd437d57a63b52d8f8ab61581ab93f
Muhammad Sayyid Rafee' Djamil <msayyid.rafeed@gmail.com>
2025-11-27 09:54:06 +0700

    style: fix auth form

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";       
-$password = "root";
+$password = "123Yesno?";
 $dbname = "db_kantin_upnvj"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
